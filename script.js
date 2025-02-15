f811f1336489039a778d3527d4571e539b7f2396
Snorlx-net <nade444yt@gmail.com>
2025-02-15 15:56:01 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+body {
+    margin: 0;
+    padding: 0;
+    font-family: Arial, sans-serif;
+    text-align: center;
+    background: url('background.jpg') no-repeat center center fixed;
+    background-size: cover;
+    color: white;
+}
 
+.container {
+    margin-top: 20%;
+}
+
+.social-links a {
+    display: block;
+    color: white;
+    font-size: 20px;
+    margin: 10px;
+    text-decoration: none;
+}
+
+.social-links a:hover {
+    color: yellow;
+}
